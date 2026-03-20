35b07026308d5259bb27f90bd84864f2923ff06e
oHMEo <96167425+oHMEo@users.noreply.github.com>
2026-03-20 14:44:40 +0100

    Expand name lists for 'lumiere' and 'ombre'

--- noms.js
+++ noms.js
@@ -1,12 +1,48 @@
 const baseDeNoms = {
     lumiere: {
-        M: ["Aethelred", "Lucidus", "Soliman", "Hyperion", "Balthazar", "Helios", "Valerius", "Caelum", "Elario", "Phébus"],
-        F: ["Auriana", "Lumina", "Séréna", "Elowen", "Galadriel", "Thalassa", "Celestia", "Astrid", "Vesta", "Lyra"],
-        Autre: ["Lux", "Zénith", "Aether", "Horizon", "Solaris", "Prisme", "Nimbus", "Eclat", "Albedo", "Phaéton"]
+        M: [
+            "Aethelvorn", "Almiren", "Berythan", "Caelidore", "Dovarian", "Elthion", "Faolun", "Gildoran", "Hestivon", "Iolaris",
+            "Jovareth", "Kaelmist", "Luvion", "Merythold", "Naluviel", "Oriastel", "Phaedron", "Quilvorn", "Rhaegast", "Solunid",
+            "Thalivius", "Urianist", "Valestor", "Wystorn", "Xalthios", "Yorathun", "Zephirant", "Aureliskan", "Belonist", "Castuvier",
+            "Darionet", "Esmaldir", "Florivon", "Galunthas", "Hadrienist", "Isidorn", "Jalior", "Kironist", "Leandris", "Maelorist",
+            "Nathiosk", "Oberonist", "Pelleast", "Quintorin", "Rohanist", "Silarist", "Tybaltian", "Ulrist", "Varianis", "Zoranith"
+        ],
+        F: [
+            "Aurellis", "Beryllia", "Caelista", "Dahlianne", "Elownys", "Faeryne", "Gildara", "Hestiala", "Iolanthe", "Junalis",
+            "Kyralis", "Luminelle", "Maelisora", "Nalianne", "Orianelle", "Phaedris", "Quillara", "Rhiannys", "Soleneira", "Thalisky",
+            "Uriellys", "Valerys", "Wystara", "Xantheline", "Yelenis", "Zephirane", "Althaelle", "Belisora", "Cassiane", "Dorianne",
+            "Esmeira", "Florienne", "Galanthelle", "Hadrielle", "Isadorae", "Jaliella", "Korranis", "Leandriska", "Maelyssa", "Nathielle",
+            "Obeline", "Pellina", "Quintelle", "Rohanelle", "Silisky", "Tybelia", "Ulricelle", "Variane", "Zoranelle", "Lioris"
+        ],
+        Autre: [
+            "Aethonne", "Berylis", "Caelumis", "Dorisko", "Elarin", "Faelis", "Gildaris", "Hesperis", "Icaris", "Jovisk",
+            "Kaelin", "Luminis", "Merylis", "Nalis", "Orianis", "Phaedis", "Quillis", "Rhaegis", "Solenis", "Thalis",
+            "Urielis", "Valeris", "Wystis", "Xanthis", "Yorathis", "Zephyris", "Aurelis", "Belenis", "Castis", "Daris",
+            "Esmonis", "Floris", "Galanis", "Hadris", "Isidis", "Jaliis", "Kiris", "Leanis", "Maelis", "Nathis",
+            "Oberis", "Pellis", "Quinis", "Rohanis", "Silis", "Tybalis", "Ulris", "Varis", "Zoris", "Eclis"
+        ]
     },
     ombre: {
-        M: ["Mortifer", "Malakor", "Obsidien", "Thanatos", "Erebus", "Vorgoth", "Drakar", "Noctis", "Sauron", "Abaddon"],
-        F: ["Morgana", "Hécate", "Nyx", "Vespera", "Lilith", "Belladona", "Séléné", "Lamia", "Morwenna", "Pandora"],
-        Autre: ["Umbra", "Néant", "Éclipse", "Abysse", "Styx", "Void", "Kael", "Obsidius", "Chaos", "Sépia"]
+        M: [
+            "Arvorth", "Blakmorn", "Corvuk", "Drakonist", "Erebon", "Falkonir", "Gorthak", "Hekost", "Ithalis", "Jorthun",
+            "Kromid", "Lethosk", "Morgonith", "Nyxorn", "Orlokist", "Phobonis", "Quaroth", "Rhadam", "Skotoni", "Tenabris",
+            "Ulgorath", "Varkast", "Woldan", "Xulath", "Ygorith", "Zalthar", "Abysson", "Banevorn", "Cthonis", "Drevol",
+            "Enyonist", "Fenrist", "Grendal", "Hadesis", "Invidis", "Jormun", "Karnith", "Lokist", "Malus", "Noxist",
+            "Ossuar", "Pyrest", "Quis", "Reven", "Sanguis", "Tartar", "Umbrus", "Vex", "Wraith", "Zol"
+        ],
+        F: [
+            "Arvortha", "Blakma", "Corvura", "Drakonis", "Ereba", "Falkia", "Gorthia", "Hekara", "Ithala", "Jorthia",
+            "Kromia", "Lethia", "Morgothia", "Nyxia", "Orlokelle", "Phobina", "Quarothis", "Rhadama", "Skotina", "Tenabrisia",
+            "Ulgorelle", "Varkia", "Woldia", "Xulalla", "Ygora", "Zalthia", "Abyssa", "Baneva", "Cthonelle", "Dreva",
+            "Enyora", "Fenra", "Grendelle", "Hadeselle", "Invidiana", "Jormuna", "Karnelle", "Lokiana", "Malise", "Noxelle",
+            "Ossuara", "Pyra", "Quisa", "Revena", "Sanguia", "Tartara", "Umbrina", "Vexia", "Wraithia", "Zolane"
+        ],
+        Autre: [
+            "Arv", "Blak", "Corv", "Drak", "Ere", "Falk", "Gorth", "Hek", "Ithal", "Jort",
+            "Kro", "Leth", "Morgo", "Nyx", "Orlo", "Phob", "Quar", "Rhada", "Skot", "Tene",
+            "Ulg", "Vark", "Wol", "Xul", "Ygo", "Zalt", "Aby", "Ban", "Ctho", "Dre",
+            "Eny", "Fen", "Gren", "Had", "Inv", "Jor", "Kar", "Lok", "Mal", "Nox",
+            "Oss", "Pyr", "Qui", "Rev", "San", "Tar", "Umb", "Vex", "Wra", "Zol", "Kael"
+        ]
     }
 };
