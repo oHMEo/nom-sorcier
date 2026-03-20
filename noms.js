9720b201d3c18fa2ba810d533c354249b5cea2e4
oHMEo <96167425+oHMEo@users.noreply.github.com>
2026-03-20 14:29:45 +0100

    Add baseDeNoms object with names categories

---
+++ noms.js
@@ -0,0 +1,12 @@
+const baseDeNoms = {
+    lumiere: {
+        M: ["Aethelred", "Lucidus", "Soliman", "Hyperion", "Balthazar", "Helios", "Valerius", "Caelum", "Elario", "Phébus"],
+        F: ["Auriana", "Lumina", "Séréna", "Elowen", "Galadriel", "Thalassa", "Celestia", "Astrid", "Vesta", "Lyra"],
+        Autre: ["Lux", "Zénith", "Aether", "Horizon", "Solaris", "Prisme", "Nimbus", "Eclat", "Albedo", "Phaéton"]
+    },
+    ombre: {
+        M: ["Mortifer", "Malakor", "Obsidien", "Thanatos", "Erebus", "Vorgoth", "Drakar", "Noctis", "Sauron", "Abaddon"],
+        F: ["Morgana", "Hécate", "Nyx", "Vespera", "Lilith", "Belladona", "Séléné", "Lamia", "Morwenna", "Pandora"],
+        Autre: ["Umbra", "Néant", "Éclipse", "Abysse", "Styx", "Void", "Kael", "Obsidius", "Chaos", "Sépia"]
+    }
+};
